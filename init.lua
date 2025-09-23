dfd3930ecd9ab33936801b30002c139d4a71859d
Alessandro Venturini <ales.ventus21@gmail.com>
2025-09-24 00:25:28 +0200

    fix setting of leader key before initializing Lazy

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-require("config.lazy")
-
 -- Set <space> as the leader key
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+require("config.lazy")
 
 vim.cmd([[
 set number
