d4e66bb7442c5e78062d588ef376884c463b79df
Alessandro Venturini <ales.ventus21@gmail.com>
2025-10-31 15:54:50 +0100

    setup: add setup script

---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+# Assuming this script is in the dotfile repo root
+DOTFILES_DIR=$(cd "$(dirname "$0")" && pwd)
+XDG_CONFIG_HOME="$HOME/.config"
+
+create_symlinks() {
+  local items=("$@")
+  for item in "${items[@]}"; do
+    IFS=':' read -r source target <<<"$item"
+    if [ -L "$target" ]; then
+      echo "Removing existing symlink $target"
+      unlink "$target"
+    elif [ -d "$target" ]; then
+      echo "Warning: $target is a directory. Skipping..."
+      continue
+    elif [ -e "$target" ]; then
+      echo "Warning: $target already exists. Skipping..."
+      continue
+    fi
+    ln -s "$DOTFILES_DIR/$source" "$target"
+    echo "Created symlink for $source"
+  done
+}
+
+common_items=(
+  "nvim:$XDG_CONFIG_HOME/nvim"
+  ".zprofile:$HOME/.zprofile"
+  ".zshrc:$HOME/.zshrc"
+)
+
+create_symlinks "${common_items[@]}"
+
+# MacOS specific setup
+if [[ "$OSTYPE" == darwin* ]]; then
+  :
+fi
+
+# Packages
+
+# install brew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install pure (prompt theme)
+# MacOS: brew install pure
+# Linux: mkdir -p "$HOME/.zsh"; git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
