cf2dd10bd162371b8ddcab21e185a71471cd9b6d
Alessandro Venturini <ales.ventus21@gmail.com>
2025-11-02 22:47:20 +0100

    nvim: better mason config
    now all LSs, linters and formatters are "ensure_installed"

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,49 @@
+local mason_package = "mason-org/mason.nvim"
+return {
+    {
+        mason_package,
+        opts = {},
+    },
+    -- Mason-lspconfig to:
+    -- - ensure_installed LSs
+    -- - auto enable LSs
+    -- We cannot use this to install linters and formatters
+    -- as it only accepts name from nvim-lspconfig
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            mason_package,
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            ensure_installed = {
+                -- Lua
+                "lua_ls",
+
+                -- Python
+                "basedpyright", -- LS
+                "ruff",         -- Linter + Formatter (partially an LS)
+
+                -- Shell
+                "bashls", -- LS
+            },
+            automatic_enable = true,
+        },
+    },
+    -- Mason-tool-installed to ensure_installed linters and formatters
+    -- We could have used this to also ensure_installed LSs but we would
+    -- loose automatic enable
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = {
+            mason_package,
+        },
+        opts = {
+            ensure_installed = {
+                -- Shell
+                "shellcheck", -- Linter
+                "shfmt",      -- Formatter
+            },
+        },
+    }
+}
