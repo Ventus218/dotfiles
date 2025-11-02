5f26b5de908bf27d58a25699ca3196963e35c96d
Alessandro Venturini <ales.ventus21@gmail.com>
2025-11-02 21:42:44 +0100

    nvim: disable shellcheck in nvim-lint
    it was already used by bash-language-server

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -7,11 +7,14 @@ return {
     config = function()
         local lint = require("lint")
         lint.linters_by_ft = {
-            sh = { "shellcheck" },
+            -- Shellcheck is run automatically by bash-language-server,
+            -- it is here just as example for future entries
+            -- sh = { "shellcheck" },
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+        -- TODO: triggering events may be adjusted
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged" }, {
             group = lint_augroup,
             callback = function()
